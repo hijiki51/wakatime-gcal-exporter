cdb736e9c3ce41f9d4ed1074386ddd592aa9dc33
hijiki51 <hibiki0719euph@gmail.com>
2022-11-08 13:39:28 +0900

    ⚡ add google api

---
+++ src/gcal.ts
@@ -0,0 +1,31 @@
+import {JSONClient} from 'google-auth-library/build/src/auth/googleauth'
+import {formatRFC3339} from 'date-fns'
+import {google} from 'googleapis'
+import {GoogleAuth} from 'google-auth-library'
+
+export const insertToGcal = async (
+  auth: GoogleAuth<JSONClient>,
+  calenderId: string,
+  colorId: string,
+  title: string,
+  start: Date,
+  end: Date
+): Promise<void> => {
+  const calender = google.calendar({version: 'v3', auth})
+  const startDate = formatRFC3339(start)
+  const endDate = formatRFC3339(end)
+
+  await calender.events.insert({
+    calendarId: calenderId,
+    requestBody: {
+      colorId,
+      summary: title,
+      start: {
+        dateTime: startDate
+      },
+      end: {
+        dateTime: endDate
+      }
+    }
+  })
+}
